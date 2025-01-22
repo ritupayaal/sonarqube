2419243df75ef12e059c546cc8c6f491f9115d5a
ritupayaal <ritu.payaal@capgemini.com>
2025-01-22 21:00:54 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+ 
+app = Flask(__name__)
+ 
+ 
+@app.route('/')
+def home():
+    """
+    home fun
+    """
+    return "<h1>Welcome to Home page<h1/>"
+ 
+ 
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
